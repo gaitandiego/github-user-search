e4699d65f09696b69951e90c392afe494fe7c72b
Diego Gaitan <gaitandiego10@gmail.com>
2024-09-28 15:21:41 -0500

    publicar app en githubPages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/github-user-search",
   plugins: [react()],
   server: {
     port: 3000
